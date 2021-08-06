f2922712071a12978e7afcc5dd6d11f02eedf0fa
Sarah Seitanakis <sseitanakis@gmail.com>
2021-08-06 13:47:24 -0400

    create a function that outputs a list of perferred ids using the node normalizer

--- reasoner_util/__init__.py
+++ reasoner_util/__init__.py
@@ -1,7 +1,11 @@
 """Build merging functions."""
 from typing import Hashable, Iterable, List, TypeVar
+from bmt import Toolkit
+from fastapi.exceptions import HTTPException
+import httpx
 
 T = TypeVar("T", bound=Hashable)
+
 
 def merge_iterables(cats1: Iterable[T], cats2: Iterable[T]) -> List[T]:
     """Merge iterables."""
@@ -10,3 +14,29 @@ def merge_iterables(cats1: Iterable[T], cats2: Iterable[T]) -> List[T]:
 
 merge_ids = merge_iterables
 merge_categories = merge_iterables
+
+
+def get_preferred_id(curie: str):
+    """Get preferred id for CURIE."""
+    params = {"curie": curie}
+    r = httpx.get("https://nodenormalization-sri-dev.renci.org/1.1/get_normalized_nodes", params=params)
+    r_dict = r.json()
+    preferred_prefix = r_dict[params["curie"]]['id']['identifier']
+    return preferred_prefix
+
+
+def get_preferred_ids(curies: List[str]):
+    """Get list of preferred ids for a list of CURIES."""
+    params = {"curies": curies}
+    r = httpx.get("https://nodenormalization-sri-dev.renci.org/1.1/get_normalized_nodes", params=params)
+    r_dict = r.json()
+    preferred_ids = [r_dict[curie]['id']['identifier'] for curie in curies]
+    return preferred_ids
+
+
+def normalize_ids(curies: List[str]):
+    """Normalize a list of ids by finding the BioLink prefered list of ids
+    and stripping out any descendants"""
+    normalized_ids = get_preferred_ids(curies)
+    # normalized_ids = strip_descendants(normalized_ids)
+    return normalized_ids
