3f5cb872599f4dd4fe13fd08404f77e96de1a2db
Sarah Seitanakis <sseitanakis@gmail.com>
2021-08-20 10:53:41 -0400

    make a strip_descendants function so that it passes the test

--- reasoner_util/__init__.py
+++ reasoner_util/__init__.py
@@ -40,19 +40,26 @@ def normalize_ids(curies: List[str]) -> List[str]:
     return normalized_ids
 
 
-def normalize_qcatagories(catagories: List[str]) -> List[str]:
+def normalize_qcategories(catagories: List[str]) -> List[str]:
     """Normalize a list of catagories by stripping all descendents"""
     normalized_catagories = strip_descendants(catagories)
     return normalized_catagories
 
 
+tk = Toolkit()
+
+
 def strip_descendants(item_list: List[str]) -> List[str]:
     """strip descendants of biolink catagories or predicates"""
+    working_list = item_list.copy()
     for x_item in item_list:
-        descendants = Toolkit.get_descendants(x_item, reflexive=False)
-        for y_item in item_list:
-            if y_item in descendants:
-                item_list.remove(y_item)
-            else:
-                pass
-    return item_list
+        descendants = tk.get_descendants(
+            x_item,
+            reflexive=False,
+            formatted=True,
+        )
+        for desnd in descendants:
+            if desnd in working_list:
+                working_list.remove(desnd)
+    stripped_list = working_list
+    return stripped_list
