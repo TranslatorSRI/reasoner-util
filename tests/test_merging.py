13f28d03bb0000796410a54bb2866806b8932e6f
Sarah Seitanakis <sseitanakis@gmail.com>
2021-09-21 15:18:52 -0400

    adjust test for merge_edges to input edges instead of messages

--- tests/test_merging.py
+++ tests/test_merging.py
@@ -48,13 +48,13 @@ def test_merge_categories():
 def test_merge_qedges():
     """Test merge_qedges"""
     with open("tests/test_jsons/test_merge_qedges1.json", "r") as file:
-        message_dict1 = json.load(file)
+        qedges1 = json.load(file)
     with open("tests/test_jsons/test_merge_qedges2.json", "r") as file:
-        message_dict2 = json.load(file)
+        qedges2 = json.load(file)
 
-    merged_message_dict = merge_qedges(message_dict1, message_dict2)
+    merged_qedges = merge_qedges(qedges1, qedges2)
 
     with open("tests/test_jsons/test_merge_qedges_success.json", "r") as file:
-        correct_merged_message_dict = json.load(file)
+        correct_merged_qedges = json.load(file)
 
-    assert merged_message_dict == correct_merged_message_dict
+    assert merged_qedges == correct_merged_qedges
